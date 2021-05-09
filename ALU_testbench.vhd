15b7a60aa948be5a35f803f0713d28ffb3208eba
twinkiedoge <abobroff8@gmail.com>
2021-05-08 21:37:16 -0400

    final testbench

--- ALU_testbench.vhd
+++ ALU_testbench.vhd
@@ -160,11 +160,74 @@ architecture sim of testbench is
     
     
     --ADD------------------------------------------------------------------------------------------------------
-   	srcB <= "00000000000000000000000000000001";
-	srcA <= "00000000000000000000000000000000";
-	command <= "0011";
+   	srcA <= "00000000000000000000000000000001";
+	srcB <= "00000000000000000000000000000000";
+	command <= "0100";
     
+    --ADD.T1 (1 + 0) (passed)
+    wait for 10 ns;
+    assert result = "00000000000000000000000000000001" report "ADD failed test 1 result: " & to_string(result);
+    assert flags = "0000" report "AND failed test 1. flags: "  & to_string(flags);
     
+    --ADD.T2 (1 + 1) (passed)
+    srcA <= "00000000000000000000000000000001";
+	srcB <= "00000000000000000000000000000001";
+    wait for 10 ns;
+    assert result = "00000000000000000000000000000010" report "ADD failed test 2 result: " & to_string(result);
+    assert flags = "0000" report "AND failed test 2. flags: "  & to_string(flags);
+    
+    --ADD.T3 (-1 + 1) (passed)
+    srcA <= "11111111111111111111111111111111";
+	srcB <= "00000000000000000000000000000001";
+    wait for 10 ns;
+    assert result = "00000000000000000000000000000000" report "ADD failed test 3 result: " & to_string(result);
+    assert flags = "0100" report "AND failed test 3. flags: "  & to_string(flags);
+    
+    --ADD.T4 (-1 + -1) (passed)
+    srcA <= "11111111111111111111111111111111";
+	srcB <= "11111111111111111111111111111111";
+    wait for 10 ns;
+    assert result = "11111111111111111111111111111110" report "ADD failed test 4 result: " & to_string(result);
+    assert flags = "1000" report "AND failed test 4. flags: "  & to_string(flags);
+    
+    --ADD.T5  (overflow and carry) (passed)
+    srcA <= "10000000000000000000000000000000";
+	srcB <= "11111111111111111111111111111110";
+    wait for 10 ns;
+    assert result = "01111111111111111111111111111110" report "ADD failed test 5 result: " & to_string(result);
+    assert flags = "0011" report "AND failed test 5. flags: "  & to_string(flags);
+    
+    --ADD.T6  (overflow) (passed)
+    srcA <= "01111111111111111111111111111111";
+	srcB <= "00000000000000000000000000000001";
+    wait for 10 ns;
+    assert result = "10000000000000000000000000000000" report "ADD failed test 6 result: " & to_string(result);
+    assert flags = "1001" report "AND failed test 6. flags: "  & to_string(flags);
+    
+    --ORR------------------------------------------------------------------------------------------------------
+    srcA <= "00000000000000000000000000000001";
+	srcB <= "00000000000000000000000000000000";
+    command <= "1100"; 
+	
+    --ORR.T1  (passed)
+    wait for 10 ns;
+    assert result = "00000000000000000000000000000001" report "ORR failed test 1 result: " & to_string(result);
+    assert flags = "0000" report "AND failed test 1. flags: "  & to_string(flags);
+    
+    --ORR.T2  (passed)
+    srcA <= "10000000000000000000000000000001";
+	srcB <= "00000000000000000000000000000001";
+    wait for 10 ns;
+    assert result = "10000000000000000000000000000001" report "ORR failed test 2 result: " & to_string(result);
+    assert flags = "1000" report "AND failed test 2. flags: "  & to_string(flags);
+    
+    --MOV------------------------------------------------------------------------------------------------------
+    srcA <= "00000000100001000000100000000001";
+	srcB <= "00000000000000000000000000000011";
+    command <= "1101"; 
+    wait for 10 ns;
+    assert result = "00000000000000000000000000000011" report "MOV failed test 1 result: " & to_string(result);
+    assert flags = "0000" report "MOV failed test 1. flags: "  & to_string(flags);
     wait;
 	
 	end process;
