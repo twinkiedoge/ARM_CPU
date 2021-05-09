9c9d8db3c00b206758ac004d5694e689edb31762
schon02 <83900125+schon02@users.noreply.github.com>
2021-05-09 14:48:35 -0400

    Create add8.vhd
    ES 4 CPU: PC+8 module

---
+++ add8.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEE.numeric_std.all;
+
+entity add8 is
+    port (
+      x : in unsigned(31 downto 0);
+      xplus8 : out unsigned(31 downto 0);
+    );
+end;
+ 
+architecture synth of add8 is 
+begin
+   xplus8 <= x + 8;
+end; 
