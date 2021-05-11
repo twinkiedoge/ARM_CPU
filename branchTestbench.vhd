443876f203dd91db9a590b4a0f240d4da16ecb4b
cgoldb05 <82987537+cgoldb05@users.noreply.github.com>
2021-05-11 02:58:13 -0400

    updated branch testbench

--- branchTestbench.vhd
+++ branchTestbench.vhd
@@ -8,46 +8,44 @@ entity branchTest is
 end branchTest;
 
 architecture synth of branchTest is
---put in branch component (maybe a preset of PC to 0)
---input instruction 
---output of PC
+component branch is
+port(
+	PC8 : in std_logic_vector(31 downto 0);
+	instruction : in std_logic_vector(31 downto 0);
+	PC : out std_logic_vector(31 downto 0)
+);
+end component;
+signal PC8 : std_logic_vector(31 downto 0);
 signal instruction : std_logic_vector(31 downto 0);
 signal PC : std_logic_vector(31 downto 0);
 begin
---instantiate branch component
+b : branch port map(PC8 => PC8, instruction => instruction, PC => PC);
 	process is
 	begin
-		--assuming PC starts at 0
-		--testing if pc changes when B = 0
+		-- imm24 = 1
 		instruction(31 downto 28) <= "1010"; -- cond
 		instruction(27 downto 26) <= "10"; -- op
-		instruction(25) <= '0'; -- funct (branch ignoring L)
-		instruction(23 downto 0) <= "000000000000000000000001";  -- imm24 
+		instruction(23 downto 0) <= "000000000000000000000001"; 
+		PC8 <= "00000000000000000000000000000000";
 		wait for 10 ns;
-		assert(PC = "000000000000000000000000000000000") report "failed" severity error;
+		assert(PC = "00000000000000000000000000000100") report to_string(PC) severity error;
 		
-		
-		--testing if pc changes when B = 1
-		instruction(25) <= '1'; -- funct (branch ignoring L)
-		instruction(23 downto 0) <= "000000000000000000000001";  -- imm24 
+		--imm24 = 1025
+		instruction(23 downto 0) <= "000000000000010000000001"; 
 		wait for 10 ns;
-		assert(PC = "000000000000000000000000000001100") report "failed" severity error;
+		assert(PC = "00000000000000000001000000000100") report "failed" severity error;
 		
 		
-		instruction(27 downto 26) <= "10"; -- op
-		instruction(25) <= '1'; -- funct (branch ignoring L)
-		instruction(23 downto 0) <= "000000000000000000011111";  -- imm24 
+		--imm24 = 31
+		PC8 <= "00000000000000000000001110000000";
+		instruction(23 downto 0) <= "000000000000000000011111"; 
 		wait for 10 ns;
-		assert(PC = "000000000000000000000000010010000") report "failed" severity error;
+		assert(PC = "00000000000000000000001111111100") report "failed" severity error;
 		
-		
-		
-		--testing negative imm24
-		instruction(27 downto 26) <= "10"; -- op
-		instruction(25) <= '1'; -- funct (branch ignoring L)
-		instruction(23 downto 0) <= "111111111111111111111010";  -- imm24 
+		--imm24 = -6
+		instruction(23 downto 0) <= "111111111111111111111010";  
 		wait for 10 ns;
-		assert(PC = "000000000000000000000000001101000") report "failed" severity error;
+		assert(PC = "00000100000000000000001101101000") report "failed" severity error;
 	wait;
 	end process;
 end synth;
