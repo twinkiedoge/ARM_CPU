ab6acf3f5d1818bb1cdcfdae175617259f24daed
cgoldb05 <82987537+cgoldb05@users.noreply.github.com>
2021-05-11 02:56:34 -0400

    updated testbench for main decoder

--- mainD_PCL_testbench.vhd
+++ mainD_PCL_testbench.vhd
@@ -14,6 +14,7 @@ port(
 	func : in std_logic_vector(5 downto 0);
     op : in std_logic_vector(1 downto 0);
     Rd : in std_logic_vector(3 downto 0);
+    B : out std_logic;
     memtoReg : out std_logic;
     memW : out std_logic;
     regW : out std_logic;
@@ -36,9 +37,10 @@ signal PCS : std_logic;
 signal Rd : std_logic_vector(3 downto 0);
 signal ALUOp : std_logic;
 signal regSrc : std_logic_vector(1 downto 0);
+signal B : std_logic;
 
 begin
-    	mD : mainDecoder port map(func => func, op => op, Rd => Rd, memtoReg => memtoReg, memW => memW, regW => regW, ALUSrc => ALUSrc, immSrc => immSrc, ALUOp => ALUOp, regSrc => regSrc, PCS => PCS);
+    	mD : mainDecoder port map(func => func, op => op, Rd => Rd, B => B, memtoReg => memtoReg, memW => memW, regW => regW, ALUSrc => ALUSrc, immSrc => immSrc, ALUOp => ALUOp, regSrc => regSrc, PCS => PCS);
 
 	process is
 	begin
@@ -46,29 +48,29 @@ begin
 		func <= "011110";
 		op <= "00";
 		wait for 10 ns;
-		assert(memtoReg = '0' and memW = '0' and ALUSrc = '0' and regW = '1' and immSrc = "00" and ALUOp <= '1' and regSrc = "00") report "failed" severity error;
+		assert(memtoReg = '0' and memW = '0' and ALUSrc = '0' and regW = '1' and immSrc = "00" and ALUOp <= '1' and regSrc = "00" and B = '0') report "failed" severity error;
 		
 		func <= "111110";
 		op <= "00";
 		wait for 10 ns;
-		assert(memtoReg = '0' and memW = '0' and ALUSrc = '1' and immSrc = "00" and regW = '1' and ALUOp <= '1' and regSrc(0) = '0') report "failed" severity error;
+		assert(memtoReg = '0' and memW = '0' and ALUSrc = '1' and immSrc = "00" and regW = '1' and ALUOp <= '1' and regSrc(0) = '0' and B = '0') report "failed" severity error;
 		
 		
 		
 		op <= "01";
 		wait for 10 ns;
-		assert(memW = '1' and ALUSrc = '1' and immSrc = "01" and regW = '0' and ALUOp <= '0' and regSrc = "10") report "failed" severity error;
+		assert(memW = '1' and ALUSrc = '1' and immSrc = "01" and regW = '0' and ALUOp <= '0' and regSrc = "10" and B = '0') report "failed" severity error;
 		
 		
 		func <= "111111";
 		op <= "01";
 		wait for 10 ns;
-		assert(memtoReg = '1' and memW = '0' and ALUSrc = '1' and immSrc = "01" and regW = '1' and ALUOp <= '0' and regSrc(0) = '0') report "failed" severity error;
+		assert(memtoReg = '1' and memW = '0' and ALUSrc = '1' and immSrc = "01" and regW = '1' and ALUOp <= '0' and regSrc(0) = '0' and B = '0') report "failed" severity error;
 		
 		
 		op <= "10";
 		wait for 10 ns;
-		assert(memtoReg = '0' and memW = '0' and ALUSrc = '1' and immSrc = "10" and regW = '0' and ALUOp <= '0' and regSrc(0) = '1') report "failed" severity error;	
+		assert(memtoReg = '0' and memW = '0' and ALUSrc = '1' and immSrc = "10" and regW = '0' and ALUOp <= '0' and regSrc(0) = '1' and B = '1') report "failed" severity error;	
 		
 		
 		--testing pc updates
